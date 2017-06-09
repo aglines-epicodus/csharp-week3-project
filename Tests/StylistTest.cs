5b02b15859d243b86911a0aad8573b8e9be51a83
Andrew Glines <aglines-epicodus@gmail.com>
2017-06-09 15:51:14 -0700

    add and fail tests for Stylist

---
+++ Tests/StylistTest.cs
@@ -0,0 +1,69 @@
+using Xunit;
+using System.Collections.Generic;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace HairSalon
+{
+  [Collection("HairSalonTests")]
+  public class StylistTest : IDisposable
+  {
+    public StylistTest()
+    {
+      DBConfiguration.ConnectionString = "Data Source=(localdb)\\mssqllocaldb;Initial Catalog=hair_salon_test;Integrated Security=SSPI;";
+    }
+
+    [Fact]
+    public void Test_StylistTestEmptyAtFirst()
+    {
+      int result = Stylist.GetAll().Count;
+      Assert.Equal(0, result);
+    }
+
+    [Fact]
+    public void Test_Equal_ReturnsTrueForSameName()
+    {
+      Stylist firstStylist = new Stylist("StylistA");
+      Stylist secondStylist = new Stylist("StylistA");
+      Assert.Equal(firstStylist, secondStylist);
+    }
+
+    [Fact]
+    public void Test_Save_SavesStylistToDatabase()
+    {
+      Stylist testStylist = new Stylist("StylistA");
+      testStylist.Save();
+      List<Stylist> result = Stylist.GetAll();
+      List<Stylist> testList = new List<Stylist>{testStylist};
+      Assert.Equal(testList, result);
+    }
+
+    [Fact]
+    public void Test_Save_AssignsIdToStylistObject()
+    {
+      Stylist testStylist = new Stylist("StylistA");
+      testStylist.Save();
+      Stylist savedStylist = Stylist.GetAll()[0];
+      int result = savedStylist.GetId();
+      int testId = testStylist.GetId();
+      Assert.Equal(testId, result);
+    }
+
+    [Fact]
+    public void Test_Find_FindsStylistInDatabase()
+    {
+      Stylist testStylist = new Stylist("StylistA");
+      testStylist.Save();
+      Stylist foundStylist = Stylist.Find(testStylist.GetId());
+      Assert.Equal(testStylist, foundStylist);
+    }
+
+    public void Dispose()
+    {
+      Stylist.DeleteAll();
+    }
+
+
+  }
+}
