4340109a3444cfb622541e1ac50cde646338e619
Andrew Glines <aglines-epicodus@gmail.com>
2017-06-10 14:19:14 -0700

    create client and stylist forms and routing

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -22,6 +22,27 @@ namespace HairSalon
         return View["clients.cshtml", allClients];
       };
 
+      Get["/stylists/new"] = _ => {
+        return View["stylists_form.cshtml"];
+      };
+
+      Post["/stylists/new"] = _ => {
+        Stylist newStylist = new Stylist(Request.Form["stylist-name"], Request.Form["stylist-contact"], Request.Form["stylist-photolink"]);
+        newStylist.Save();
+        return View["success.cshtml"];
+      };
+
+      Get["/clients/new"] = _ => {
+        List<Stylist> AllStylists = Stylist.GetAll();
+        return View["clients_form.cshtml", AllStylists];
+      };
+
+      Post["/clients/new"] = _ => {
+        Client newClient = new Client(Request.Form["client-name"], Request.Form["stylist-id"], Request.Form["client-contact"]);
+        newClient.Save();
+        return View["success.cshtml"];
+      };
+
     }
   }
 }
